9363657ef266d003304d9b35af1e2c53120a3471
Ajahruddin-Alam <108749672+Ajahruddin-Alam@users.noreply.github.com>
2025-08-14 16:44:44 +0530

    Push to main

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function btnMail(){
     alert("Your Email Send Thanks !!")
 }
+
+// Scroll animation
+const scrollElements = document.querySelectorAll('.animate');
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add('active');
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+});
